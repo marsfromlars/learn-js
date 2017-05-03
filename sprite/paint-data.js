588836fbdf54f6a0c79de500cf003c08ff22a1c1
larsfrommars <larsfrommars@gmail.com>
2017-05-03 11:54:28 +0200

    w.i.p.

---
+++ sprite/paint-data.js
@@ -0,0 +1,16 @@
+let b = '#000000';
+let w = '#FFFFFF';
+let data = [ 
+    b, b, b, b, b, b, b, b,
+    b, w, b, b, b, w, w, b,
+    b, b, w, b, b, b, w, b,
+    b, b, b, w, b, b, b, b,
+    b, b, b, b, w, b, b, b,
+    b, w, b, b, b, w, b, b,
+    b, w, w, b, b, b, w, b,
+    b, b, b, b, b, b, b, b
+    ];
+
+let canvas1 = document.getElementById( 'canvas1' )
+
+
